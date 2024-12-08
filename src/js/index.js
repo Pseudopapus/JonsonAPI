e55393c1595b087e7b6ded172aba6644f18c7b93
McMaldo <maldonado.ignacio.pablo@gmail.com>
2024-12-08 18:48:35 -0300

    FEAT #0000 * code tags * code theme import (highlight.js)

--- src/js/index.js
+++ src/js/index.js
@@ -4,14 +4,14 @@ fetch('./src/data/data.json')
 .then(response=> {return response.json()})
 .then(data=>{
   data.hello.forEach(e=>{
-    box.innerHTML +=`
+    box.innerHTML += /*html*/`
     <article class="lang">
       <div class="name">
         <h2 class="center">${e.name}</h2>
         <h4 class="center">.${e.ext}</h4>
       </div>
       <div class="code">
-        ${e.code}
+        <pre><code class="${e.class}">${e.code}</code></pre>
       </div>
     </article>`
   })
